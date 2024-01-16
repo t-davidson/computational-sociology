43301e9b8c8a6accdfdae934e31a82898853fce9
t-davidson <thomas.davidson@rutgers.edu>
2024-01-16 12:12:32 -0500

    Adding coding error problem for l1

---
+++ 2024/course-materials/code/l1_problem_1.R
@@ -0,0 +1,9 @@
+# Fix the code on lines 3-6
+
+calculate_mean <- function() {
+    mean <- sum(x) / len(x)
+    return()
+}}
+
+# This line should run once the code works. What is your answer?
+calculate_mean(rnorm(1000))
